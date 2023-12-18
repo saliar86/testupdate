b7da11578cac0ec3a0ddd21ed2858bc9f1a76837
Salar Khosravi <salar.khosravi@gmail.com>
2023-12-18 22:42:56 +0330

    change to update Module

---
+++ updatefunc.php
@@ -0,0 +1,51 @@
+<?php
+
+// URL of the zip file
+$zipFileUrl = 'https://dev.frenchexam.ir/codes.zip';
+
+// Local path to save the zip file
+$localZipFilePath = __DIR__ . '/codes.zip';
+
+// Local directory to unzip the contents
+$localExtractPath = __DIR__ . '/new';
+
+// Initialize cURL session
+$ch = curl_init($zipFileUrl);
+$fp = fopen($localZipFilePath, 'wb');
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// Execute cURL session
+curl_exec($ch);
+
+// Check for cURL errors
+if (curl_errno($ch)) {
+    echo 'Curl error: ' . curl_error($ch);
+} else {
+    echo 'Zip file downloaded successfully.';
+}
+
+// Close cURL session and file pointer
+curl_close($ch);
+fclose($fp);
+
+// Check if the zip file was downloaded successfully
+if (file_exists($localZipFilePath)) {
+    // Open the zip file
+    $zip = new ZipArchive;
+    if ($zip->open($localZipFilePath) === TRUE) {
+        // Extract the contents to the local directory
+        $zip->extractTo($localExtractPath);
+        $zip->close();
+
+        echo 'Zip file extracted successfully.';
+    } else {
+        echo 'Failed to open the zip file.';
+    }
+} else {
+    echo 'Failed to download the zip file.';
+}
+
+?>
